c98a533f39d1955613c1658e4c9faeec35232f4d
Zain Rehmani <zmrehmani@gmail.com>
2015-04-17 22:29:58 -0400

    Added request extensio page

---
+++ LMS/src/lms/RequestExtensionPageController.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lms;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author zain
+ */
+public class RequestExtensionPageController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @FXML
+    private Button submitButton;
+    @FXML
+    private TextField issueID;
+    @FXML
+    private TextField currentExtensionDate;
+    @FXML
+    private TextField currentReturnDate;
+    @FXML
+    private TextField originalCheckoutDate;
+    @FXML
+    private TextField newExtensionDate;
+    @FXML
+    private TextField newReturnDate;
+    @FXML
+    private Button submitExtensionRequest;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
