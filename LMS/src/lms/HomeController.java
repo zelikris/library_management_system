cc4e58914aa22279930301fd78fbd1ba85d2736c
David Keatts <david.keatts@gmail.com>
2015-04-21 00:12:06 -0400

    disable checkout and returns unless user is staff, add logout button

--- LMS/src/lms/HomeController.java
+++ LMS/src/lms/HomeController.java
@@ -7,12 +7,18 @@ package lms;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -23,13 +29,67 @@ import javafx.stage.Stage;
  */
 public class HomeController implements Initializable {
 
+    @FXML Button checkOutButton;
+    @FXML Button returnButton;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        if (userIsStaff()) {
+            checkOutButton.setDisable(false);
+            returnButton.setDisable(false);
+        } else {
+            checkOutButton.setDisable(true);
+            returnButton.setDisable(true);
+        }
     }    
+    
+    private Boolean userIsStaff() {
+        Connection con = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection("jdbc:mysql://academic-mysql.cc.gatech.edu/cs4400_Group_22", "cs4400_Group_22",
+            "ayt2V3Ck");
+
+            Statement stmt = con.createStatement();
+            ResultSet results = stmt.executeQuery("SELECT * FROM STAFF\n" +
+                                                  "WHERE Staff_username = '" + LMS.getSessionUser() + "'");
+            while (results.next()) {
+                return true;
+            }
+        } catch(ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException e) {
+            System.err.println("Exception: " + e.getMessage());
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch(SQLException e) {
+                System.err.println(e);
+            }
+        }
+        return false;
+    }
+    
+    @FXML
+    private void onBackEvent() {
+        try {
+            Parent foster = LMS.getParent();
+            Stage stage = LMS.getStage();
+            foster = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+
+            Scene scene = new Scene(foster);
+
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     
     @FXML
     private void onSearchEvent(MouseEvent event) {
